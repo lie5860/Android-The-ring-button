031d8a3d04c82d692d72aad103b84c3a2d5ac1a2
QHT <1003077897@qq.com>
2016-08-31 11:53:15 +0800

    android自定义view----环形按钮实现

---
+++ src/com/example/opendor/TrapezoidImageButton.java
@@ -0,0 +1,53 @@
+package com.example.opendor;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+
+public class TrapezoidImageButton extends ImageView {
+
+    public TrapezoidImageButton(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+    }
+
+    public TrapezoidImageButton(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    }
+
+    public TrapezoidImageButton(Context context) {
+    super(context);
+    }
+    
+    @SuppressLint("ClickableViewAccessibility") @Override
+    public boolean onTouchEvent(MotionEvent event) {
+    if (isTouchPointInView(event.getX(),event.getY())||event.getAction() != MotionEvent.ACTION_DOWN){
+        return super.onTouchEvent(event);
+    }else{
+        return false;
+    }
+    }
+
+    protected boolean isTouchPointInView(float localX, float localY){
+    Bitmap bitmap = Bitmap.createBitmap(getWidth(), getHeight(), Config.ARGB_8888);
+    Canvas canvas = new Canvas(bitmap);
+    draw(canvas);
+    int x = (int)localX;
+    int y = (int)localY;
+    if (x < 0 || x >= getWidth())
+        return false;
+    if (y < 0 || y >=	 getHeight())
+        return false;
+    int pixel = bitmap.getPixel(x,y);
+    if ((pixel&0xff000000) != 0){ 
+        return true;
+    }else{
+        return false; 
+    }
+    }
+}
